1e5d4fc4a54ed7aed1c014bafed32a245783e157
mohsen.ansari <mohsen.ansari.bim@gmail.com>
2022-09-06 19:19:08 +0430

    Add project files.

---
+++ Cinema_API/Controllers/MoviesController.cs
@@ -0,0 +1,62 @@
+﻿using Cinema_API.Data;
+using Cinema_API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Cinema_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private CinemaDbContext _dbContext;
+        public MoviesController(CinemaDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        // GET: api/<MoviesController>
+        [HttpGet]
+        public IEnumerable<Movie> Get()
+        {
+            return _dbContext.Movies.ToList();
+        }
+
+        // GET api/<MoviesController>/5
+        [HttpGet("{id}")]
+        public Movie Get(int id)
+        {
+            return _dbContext.Movies.Where(m => m.Id == id).First();
+        }
+
+        // POST api/<MoviesController>
+        [HttpPost]
+        public void Post([FromBody] Movie movie)
+        {
+            _dbContext.Movies.Add(movie);
+            _dbContext.SaveChanges();
+        }
+
+        // PUT api/<MoviesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Movie movie)
+        {
+            var movieToChange = _dbContext.Movies.Where(mov => mov.Id == id).First();
+            movieToChange.Language = movie.Language;
+            movieToChange.Name = movie.Name;
+            _dbContext.SaveChanges();
+        }
+
+        // DELETE api/<MoviesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _dbContext.Movies.Remove(_dbContext.Movies.Where(mov => mov.Id == id).First());
+            _dbContext.SaveChanges();
+        }
+    }
+}
---
+++ Cinema_API/Data/CinemaDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Cinema_API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cinema_API.Data
+{
+    public class CinemaDbContext: DbContext
+    {
+        public CinemaDbContext(DbContextOptions<CinemaDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Movie> Movies{ get; set; }
+
+    }
+}
