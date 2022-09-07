019f9aa9d681e3e3e596b034af87abec7aa25bfe
mohsen.ansari <mohsen.ansari.bim@gmail.com>
2022-09-07 16:58:40 +0430

    validation

--- Cinema_API/Models/Movie.cs
+++ Cinema_API/Models/Movie.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,12 +10,18 @@ namespace Cinema_API.Models
 {
     public class Movie
     {
+        //just to make sure we are not getting any empty or null information from the client
         public int Id { get; set; }
+        [Required(ErrorMessage ="Name cannot be null or empty")]
+        //you can fill other properties with empty or null errors too.
         public string Language { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public double Rating { get; set; }
         [NotMapped]
         public IFormFile Image { get; set; }
+        [Required]
         public string ImageUrl { get; set; }
     }
 }
